7a813d3e5e8dcd531fc257359a68f73ec2bf855d
Catherine <catherinecharlesfesto@gmail.com>
2025-05-01 19:52:07 +0300

    Sum of the natural numbers

---
+++ main.py
@@ -0,0 +1,9 @@
+#Write a Python program for printing the sum of the first ten natural numbers using the while loop.
+
+i=1
+sum=0
+while(i<=10):
+    sum=sum+i
+    i=i+1
+    
+print("the sum of the first nutural number",sum)
